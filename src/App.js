5ee5b1fb99aa8bb2267b14b301129d0582dd5e94
sangeeta2003 <mishrasangeeta944@gmail.com>
2023-12-07 08:00:54 +0530

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
-
 function App() {
   return (
-    <div className="App">
-      
+    <div className="bg-white">
+      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+      <p className="text-gray-600">This is a simple React application with Tailwind CSS.</p>
     </div>
   );
 }
